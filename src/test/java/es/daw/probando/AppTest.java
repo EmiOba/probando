5059e3a02e0458bcf3b57849db00043e54c44db3
EmiOba <obandokey18@gmail.com>
2024-02-22 15:29:38 +0100

    primer commit

---
+++ src/test/java/es/daw/probando/AppTest.java
@@ -0,0 +1,18 @@
+package es.daw.probando;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
